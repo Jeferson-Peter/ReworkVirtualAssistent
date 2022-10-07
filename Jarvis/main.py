56902de41ce9f5735d88f7bf1646fe4e94fed61f
Jeferson <jefersonpeter32@gmail.com>
2022-10-06 21:12:39 -0300

    Main Recognition

--- Jarvis/main.py
+++ Jarvis/main.py
@@ -1,27 +1,48 @@
-import speech_recognition
+import speech_recognition as sr
+import pyttsx3
+
+# Initialize the recognizer
+r = sr.Recognizer()
 
 
-def voice_recognition() -> None:
-    # Microfone
-    mic = speech_recognition.Recognizer()
-    # Ouvindo o mic
-    while True:
-        with speech_recognition.Microphone() as source:
-            # mic.adjust_for_ambient_noise(source)
-            audio = mic.listen(source)
-        try:
-            # armazena o texto na variavel phrase
-            phrase = mic.recognize_google(audio, language='pt-BR')
-            # se tiver phrase imprime ela
-            if phrase != None:
-                yield "Voce disse: " + phrase
-                break
-        # Caso nao reconheca o audio
-        except speech_recognition.RequestError:
-            # Imprime que nao entendeu
-            yield "Assistente: Nao foi possivel entender"
-            break
-        except speech_recognition.UnknownValueError:
-            # Imprime que nao entendeu
-            yield "Assistente: Nao foi possivel entender"
-            break
+# Function to convert text to
+# speech
+def SpeakText(command):
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
+
+
+# Loop infinitely for user to
+# speak
+
+while (1):
+
+    # Exception handling to handle
+    # exceptions at the runtime
+    try:
+
+        # use the microphone as source for input.
+        with sr.Microphone() as source2:
+
+            # wait for a second to let the recognizer
+            # adjust the energy threshold based on
+            # the surrounding noise level
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+
+            # listens for the user's input
+            audio2 = r.listen(source2)
+
+            # Using google to recognize audio
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
+
+            print("Did you say " + MyText)
+            SpeakText(MyText)
+
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+
+    except sr.UnknownValueError:
+        print("unknown error occured")
