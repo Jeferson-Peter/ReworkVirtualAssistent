6bbaba39c622cc62dfd7827bd08d1b641bb7484d
Jeferson <jefersonpeter32@gmail.com>
2022-10-12 15:17:07 -0300

    Whole Refactor on open web applications and implemented some os apps

--- jarvis.py
+++ jarvis.py
@@ -12,6 +12,7 @@ from youtube import youtube
 from sys import platform
 import os
 import getpass
+from open_applications import OpenApplications
 
 ## todo: 
 ## pesquisa por voz
@@ -29,7 +30,8 @@ import getpass
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
- 
+engine.setProperty('rate', 200)
+
 # this method is for taking the commands
 # and recognizing the command from the
 # speech_Recognition module we will use
@@ -97,7 +99,7 @@ class Jarvis:
             self.chrome_path = 'open -a /Applications/Google\ Chrome.app'
 
         elif platform == "win32":
-            self.chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+            self.chrome_path = 'C:\Program Files (x86)\Google\ChromeDriver\chromedriver.exe'
         else:
             print('Unsupported OS')
             exit(1)
@@ -107,9 +109,9 @@ class Jarvis:
 
     def wishMe(self) -> None:
         hour = int(datetime.datetime.now().hour)
-        if hour >= 0 and hour < 12:
+        if 0 <= hour < 12:
             speak("Good Morning SIR")
-        elif hour >= 12 and hour < 18:
+        elif 12 <= hour < 18:
             speak("Good Afternoon SIR")
 
         else:
@@ -118,64 +120,38 @@ class Jarvis:
         weather()
         speak('I am JARVIS. Please tell me how can I help you SIR?')
 
+
     def execute_query(self, query):
-        print(query)
         if 'Optical Text Recognition' or 'Text Recognition' in query:
             pass
         if 'jarvis are you there' in query:
-            print()
             speak("Yes Sir, at your service")
         if 'jarvis who made you' in query:
             speak("Yes Sir, my master build me in AI")
-
-        elif 'cpu' in query:
-            cpu()
-
-        elif 'joke' in query:
-            joke()
-
-        elif 'screenshot' in query:
-            speak("taking screenshot")
-            screenshot()
-
-        elif 'open youtube' in query:
-
-            webbrowser.get('chrome').open_new_tab('https://youtube.com')
-
-        elif 'open amazon' in query:
-            webbrowser.get('chrome').open_new_tab('https://amazon.com')
-
-        elif 'open google' in query:
-            webbrowser.get('chrome').open_new_tab('https://google.com')
-
-        elif 'open stackoverflow' in query:
-            webbrowser.get('chrome').open_new_tab('https://stackoverflow.com')
-
+        elif 'open' in query:
+            application = OpenApplications(query)
+            application.parse_commands()
         elif 'play music' in query:
             os.startfile("D:\\RoiNa.mp3")
-
         elif 'search youtube' in query:
             speak('What you want to search on Youtube?')
             youtube(takeCommand())
-        elif 'the time' in query:
+        elif 'what time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
             speak(f'Sir, the time is {strTime}')
-
         elif 'search' in query:
             speak('What do you want to search for?')
             search = takeCommand()
             url = 'https://google.com/search?q=' + search
-            webbrowser.get('chrome').open_new_tab(
+            webbrowser.open_new_tab(
                 url)
             speak('Here is What I found for' + search)
-
         elif 'location' in query:
             speak('What is the location?')
             location = takeCommand()
             url = 'https://google.nl/maps/place/' + location + '/&amp;'
             webbrowser.get('chrome').open_new_tab(url)
             speak('Here is the location ' + location)
-
         elif 'your master' in query:
             if platform == "win32" or "darwin":
                 speak('I have a team who created me. They created me a couple of days ago')
@@ -187,7 +163,6 @@ class Jarvis:
             speak('My name is JARVIS')
         elif 'who made you' in query:
             speak('I was created by my AI master in 2022')
-
         elif 'stands for' in query:
             speak('J.A.R.V.I.S stands for JUST A RATHER VERY INTELLIGENT SYSTEM')
         elif 'open code' in query:
@@ -196,29 +171,23 @@ class Jarvis:
                     "C:\\Users\\gs935\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe")
             elif platform == "linux" or platform == "linux2" or "darwin":
                 os.system('code .')
-
         elif 'shutdown' in query:
             if platform == "win32":
                 os.system('shutdown /p /f')
             elif platform == "linux" or platform == "linux2" or "darwin":
                 os.system('poweroff')
-
         elif 'cpu' in query:
             cpu()
         elif 'your friend' in query:
             speak('My friends are Google assistant alexa and siri')
-
+        elif 'cpu' in query:
+            cpu()
         elif 'joke' in query:
             joke()
-
         elif 'screenshot' in query:
             speak("taking screenshot")
             screenshot()
-
-        elif 'github' in query:
-            webbrowser.get('chrome').open_new_tab(
-                'https://github.com/gauravsingh9356')
-
+            speak("screenshot taken")
         elif 'remember that' in query:
             speak("what should i remember sir")
             rememberMessage = takeCommand()
@@ -226,25 +195,14 @@ class Jarvis:
             remember = open('data.txt', 'w')
             remember.write(rememberMessage)
             remember.close()
-
         elif 'do you remember anything' in query:
             remember = open('data.txt', 'r')
             speak("you said me to remember that" + remember.read())
-
         elif 'sleep' in query:
             sys.exit()
-
         elif 'dictionary' in query:
             speak('What you want to search in your intelligent dictionary?')
             translate(takeCommand())
-        # elif 'voice' in query:
-        #     print('Query: ' + query)
-        #     if 'female' in query:
-        #         engine.setProperty('voice', voices[1].id)
-        #     else:
-        #         engine.setProperty('voice', voices[2].id)
-        #     speak("Hello Sir, I have switched my voice. How is it?")
-
         elif 'convert currency' in query:
             speak("What's the value you want to convert?")
             value = takeCommand()
